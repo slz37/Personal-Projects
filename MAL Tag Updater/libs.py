6c440e9c35a20d477c22b28aed0529eb154811c7
Salvatore Zerbo <slz37@drexel.edu>
2019-03-25 14:24:48 -0400

    Initial commit

---
+++ MAL Tag Updater/libs.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+import re, sys, time
+
+from tag_methods import *
+
