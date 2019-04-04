22ea510c4065d282dfc81a5cbbd9aaa86cf2d6f1
slz37 <slz37@drexel.edu>
2019-04-04 17:42:59 -0400

    Removed files

--- MAL Bot/libs.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-
-import re, sys, time
-
-from anime import *
-from tag import *
-from rank import *
